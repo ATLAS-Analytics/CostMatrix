af197efb1b870968594e77fa87a6109eecc5fdeb
Ilija Vukotic <ivukotic@cern.ch>
2018-06-11 15:53:14 -0500

    with dns and probe

---
+++ HC/probe.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import requests
+import time
+import json as simplejson
+
+ts = int(round(time.time() * 1000))
+
+server = 'http://costmatrix.slateci.net:8080/'
+
+print('--------------------------------- Uploading result ---------------------------------')
+data = dict(source="SITE_FROM", destination="SITE_TO", rate=123.456, time=ts)
+u = requests.post(server, params=data)
+print(u.text)
+
+event = {}
+event['source'] = "SITE_FROM"
+event['destination'] = "SITE_TO"
+event['log'] = 'asdfasdf sdfasdf  asdfd'
+event['time'] = ts
+u = requests.post(server + 'error/', params=event)
+# req.add_header('Content-Type', 'application/json')
+print(u.text)
